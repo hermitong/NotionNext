efc4cfa01b338eded049520991fdad6ed2c21746
Yintong Zheng <yintong.zheng@uzh.ch>
2025-01-24 01:44:32 +0100

    修改描述和格式

--- themes/simple/config.js
+++ themes/simple/config.js
@@ -3,7 +3,7 @@ const CONFIG = {
   SIMPLE_LOGO_IMG: '/Logo.webp',
   SIMPLE_TOP_BAR: true, // 显示顶栏
   SIMPLE_TOP_BAR_CONTENT: process.env.NEXT_PUBLIC_THEME_SIMPLE_TOP_TIPS || '',
-  SIMPLE_LOGO_DESCRIPTION: process.env.NEXT_PUBLIC_THEME_SIMPLE_LOGO_DESCRIPTION || '<div>旅居欧洲，业余交易<br/>读书写字，记录生活<br/>———————— ————————<br/>「做一个世界的水手<br/>去所有未经的码头」</div>',
+  SIMPLE_LOGO_DESCRIPTION: process.env.NEXT_PUBLIC_THEME_SIMPLE_LOGO_DESCRIPTION || '<div>旅居欧洲，业余交易<br/>读书写字，记录生活<br/>———————— ————————<br/>「做一个世界的水手<br/>  去所有未经的码头」</div>',
 
   SIMPLE_AUTHOR_LINK: process.env.NEXT_PUBLIC_AUTHOR_LINK || '#',
 
